d4581497b8fef60a3698d8621fda5aad38280585
KBAHN_NOTE <KBAHN_NOTE@DESKTOP-A41DHR9>
2017-01-17 13:56:32 +0900

    패스워드 유효성 comment 수정

--- underdog/src/main/webapp/resources/js/member.js
+++ underdog/src/main/webapp/resources/js/member.js
@@ -251,17 +251,17 @@ $(function () {
 		var spe = pw.search(/[`~!@@#$%^&*|₩₩₩'₩";:₩/?]/gi);
 		if(pw.length < 8 || pw.length > 20){
 			$("#txt_pw").css({"color": "red"});
-			$("#txt_pw").text("8자리 ~ 20자리 이내로 입력해주세요.");
+			$("#txt_pw").text("영문,숫자,특수문자 8~20자리 입력");
 			return false;
 		}
 		if(pw.search(/₩s/) != -1){
 			$("#txt_pw").css({"color": "red"});
-			$("#txt_pw").text("비밀번호는 공백업이 입력해주세요.");
+			$("#txt_pw").text("비밀번호는 공백없이 입력해주세요.");
 			return false;
 		} 
 		if(num < 0 || eng < 0 || spe < 0 ){
 			$("#txt_pw").css({"color": "red"});
-			$("#txt_pw").text("영문,숫자, 특수문자를 혼합하여 입력해주세요.");
+			$("#txt_pw").text("영문,숫자,특수문자 8~20자리 입력");
 			return false;
 		}
 		$("#txt_pw").css({"color": "blue"});
