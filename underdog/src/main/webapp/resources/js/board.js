8aec8ddf52b41f3b910d38949d7b0fa372d6a7b3
Administrator <Administrator@WIN-5NBT9MAD6O7>
2017-01-17 14:44:24 +0900

    Q&A list, read, write 작성 / board.js 추가

---
+++ underdog/src/main/webapp/resources/js/board.js
@@ -0,0 +1,25 @@
+﻿
+//-------------------Q&A 유효성 검사(임시)-------------------------
+function board_check() {
+	if ($.trim($("#board_name").val()) == "") {
+		alert("글쓴이를 입력하세요!");
+		$("#board_name").val("").focus();
+		return false;
+	}
+	if ($.trim($("#board_pass").val()) == "") {
+		alert("비밀번호를 입력하세요!");
+		$("#board_pass").val("").focus();
+		return false;
+	}
+	if ($.trim($("#board_subject").val()) == "") {
+		alert("글제목을 입력하세요!");
+		$("#board_subject").val("").focus();
+		return false;
+	}
+	if ($.trim($("#board_content").val()) == "") {
+		alert("글내용을 입력하세요!");
+		$("#board_content").val("").focus();
+		return false;
+	}
+}
+//-------------------Q&A 유효성 검사(임시) end-------------------------
