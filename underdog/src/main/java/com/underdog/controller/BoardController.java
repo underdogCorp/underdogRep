f4aa2a5d562f7d3077380fae35f5d1a10d2e7fc3
Administrator <Administrator@MSDN-SPECIAL>
2017-01-17 15:51:14 +0900

    12

--- underdog/src/main/java/com/underdog/controller/BoardController.java
+++ underdog/src/main/java/com/underdog/controller/BoardController.java
@@ -1,6 +1,6 @@
 package com.underdog.controller;
 
-<<<<<<< HEAD
+
 import javax.inject.Inject;
 
 import org.slf4j.Logger;
@@ -10,9 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.underdog.service.BoardService;
 
-@Controller
+
 // @RequestMapping("/board/*")
-=======
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
