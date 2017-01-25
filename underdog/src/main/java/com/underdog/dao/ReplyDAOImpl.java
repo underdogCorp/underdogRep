624853e03327d390d4ada939f27780356d5d76fc
Administrator <Administrator@WIN-5NBT9MAD6O7>
2017-01-25 11:31:43 +0900

    zzz

--- underdog/src/main/java/com/underdog/dao/ReplyDAOImpl.java
+++ underdog/src/main/java/com/underdog/dao/ReplyDAOImpl.java
@@ -1,5 +1,8 @@
 package com.underdog.dao;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class ReplyDAOImpl implements ReplyDAO {
 
 }
--- underdog/src/main/java/com/underdog/service/ReplyServiceImpl.java
+++ underdog/src/main/java/com/underdog/service/ReplyServiceImpl.java
@@ -1,5 +1,8 @@
 package com.underdog.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ReplyServiceImpl implements ReplyService {
 
 }
